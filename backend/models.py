0566718e6ef367e2a8b4f1c1a5f019e54e9ee381
DANIEL OJULU <uludan99@gmail.com>
2026-04-09 16:40:25 +0300

    frontend

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Appointment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    phone = db.Column(db.String(20))
-    message = db.Column(db.Text)
--- backend/routes/contact.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-contact_bp = Blueprint('contact', __name__)
-
-@contact_bp.route('/api/contact', methods=['POST'])
-def contact():
-    data = request.json
-    return jsonify({'message': 'Message received'})
